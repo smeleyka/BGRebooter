91b657288a6e8a83a954bb10f32f277679505088
smeleyka <smeleyka@gmail.com>
2018-10-17 18:07:48 +0300

    RecyclerView Onclick

--- app/src/main/java/ru/smeleyka/bgrebooter/view/fragments/HostgroupFragment.java
+++ app/src/main/java/ru/smeleyka/bgrebooter/view/fragments/HostgroupFragment.java
@@ -1,17 +1,14 @@
 package ru.smeleyka.bgrebooter.view.fragments;
 
-import android.app.Activity;
 import android.app.Fragment;
 import android.content.Context;
 import android.os.Bundle;
-import android.support.annotation.MainThread;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -25,7 +22,6 @@ import io.reactivex.Observable;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import ru.smeleyka.bgrebooter.R;
 import ru.smeleyka.bgrebooter.model.entity.HostgroupGetResponse;
-import ru.smeleyka.bgrebooter.view.MainActivity;
 import ru.smeleyka.bgrebooter.view.MainActivityView;
 
 public class HostgroupFragment extends Fragment {
@@ -67,7 +63,6 @@ public class HostgroupFragment extends Fragment {
         }
         recyclerView.setLayoutManager(mLinearLayoutManager);
         recyclerView.setAdapter(myRecyclerViewAdapter);
-        recyclerView.addOnItemTouchListener();
     }
 
     public void addItemToRecyclerView(HostgroupGetResponse.Hostgroup hostgroup) {
@@ -78,8 +73,7 @@ public class HostgroupFragment extends Fragment {
         myRecyclerViewAdapter.setItemSubscription(hostgroupObservable);
     }
 
-
-    class MyRecyclerViewAdapter extends RecyclerView.Adapter<MyViewHolder> {
+    class MyRecyclerViewAdapter extends RecyclerView.Adapter<MyViewHolder> implements View.OnClickListener {
         private String TAG = "MyRecyclerViewAdapter.class";
 
         List<HostgroupGetResponse.Hostgroup> hostgroupList;
@@ -119,11 +113,7 @@ public class HostgroupFragment extends Fragment {
         public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
             View view = LayoutInflater.from(mContext)
                     .inflate(R.layout.recycler_view_item_two_line, parent, false);
-
-            Log.d(TAG, "Class of view: " + view.getClass().toString());
-            Log.d(TAG, "LayoutParams of view is null: " + (view.getLayoutParams() == null));
-
-            return new MyViewHolder(view);
+            return new MyViewHolder(view,myRecyclerViewAdapter);
         }
 
         @Override
@@ -136,24 +126,37 @@ public class HostgroupFragment extends Fragment {
             return hostgroupList.size();
         }
 
+        @Override
+        public void onClick(View v) {
+            MyViewHolder viewHolder = (MyViewHolder) v.getTag();
+            int position = viewHolder.getLayoutPosition();
+            mainActivityView.showError("Clicked " + hostgroupList.get(position).getName());
+        }
     }
 
     class MyViewHolder extends RecyclerView.ViewHolder {
         private String TAG = "MyViewHolder.class";
+        //private MyRecyclerViewAdapter myRecyclerViewAdapter;
 
         TextView groupeNameTextView;
         TextView groupeIdTextView;
         TextView groupeHostsCountTextView;
         TextView groupeImgTextView;
 
-        public MyViewHolder(View itemView) {
+        public MyViewHolder(View itemView, MyRecyclerViewAdapter myRecyclerViewAdapter) {
             super(itemView);
+
+            //this.myRecyclerViewAdapter=myRecyclerViewAdapter;
+
             Log.d(TAG, "Class of view: " + itemView.getClass().toString());
             Log.d(TAG, "LayoutParams of view is null: " + (itemView.getLayoutParams() == null));
             groupeNameTextView = itemView.findViewById(R.id.recycler_item_text_first);
             groupeIdTextView = itemView.findViewById(R.id.recycler_item_text_groupe_id);
             groupeHostsCountTextView = itemView.findViewById(R.id.recycler_item_text_host_count);
             groupeImgTextView = itemView.findViewById(R.id.recycler_item_image);
+            itemView.setTag(this);
+            itemView.setOnClickListener(myRecyclerViewAdapter);
+
         }
 
         public void bind(HostgroupGetResponse.Hostgroup hostgroup) {
@@ -165,29 +168,6 @@ public class HostgroupFragment extends Fragment {
 
         }
     }
-
-    class MyRecyclerViewOnItemTouchListener extends RecyclerView.SimpleOnItemTouchListener {
-
-        public MyRecyclerViewOnItemTouchListener() {
-            super();
-        }
-
-        @Override
-        public boolean onInterceptTouchEvent(RecyclerView rv, MotionEvent e) {
-            return super.onInterceptTouchEvent(rv, e);
-        }
-
-        @Override
-        public void onTouchEvent(RecyclerView rv, MotionEvent e) {
-            super.onTouchEvent(rv, e);
-        }
-
-        @Override
-        public void onRequestDisallowInterceptTouchEvent(boolean disallowIntercept) {
-            super.onRequestDisallowInterceptTouchEvent(disallowIntercept);
-        }
-
-     }
 
 
 }
