34595df25c04507e5f34740ed64626ba53edffa5
smeleyka <smeleyka@gmail.com>
2018-10-24 18:10:46 +0300

    HostFragment //to do... - fragment backstack

---
+++ app/src/main/java/ru/smeleyka/bgrebooter/contract/HostGroupContract.java
@@ -0,0 +1,15 @@
+package ru.smeleyka.bgrebooter.contract;
+
+public interface HostGroupContract {
+
+    public interface HostGroupFragment{
+
+
+
+    }
+
+
+    public interface HostGroupePresenter{
+
+    }
+}
